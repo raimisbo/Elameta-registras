4ed7f0e73fe3f4bf26dc5b87199a7f2ff7528efc
raimisbosikis <bosikis@gmail.com>
2025-07-29 20:07:10 +0300

    Snippetai: kainos_lentele, messsages, uzklausa_action

--- detaliu_registras/templatetags/uzklausu_tagai.py
+++ detaliu_registras/templatetags/uzklausu_tagai.py
@@ -2,6 +2,6 @@ from django import template
 
 register = template.Library()
 
-@register.inclusion_tag('snippets/uzklausa_blokas.html')
+@register.inclusion_tag('detaliu_registras/components/uzklausa_blokas.html')
 def rodyti_uzklausa(uzklausa):
     return {'uzklausa': uzklausa}

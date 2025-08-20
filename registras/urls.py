7857741a1898fd9cdaad0c2b893ed6a9d8a1c95b
raimisbosikis <bosikis@gmail.com>
2025-08-20 11:53:59 +0300

    Su filtrais uzklausu lango, be filtru pirmam lange

--- registras/urls.py
+++ registras/urls.py
@@ -1,31 +1,23 @@
-"""
-URL configuration for registras project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.views.generic import RedirectView
+from rest_framework import routers
+from detaliu_registras.api import DetaleViewSet
+
+router = routers.DefaultRouter()
+router.register(r"detales", DetaleViewSet, basename="detales")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('detaliu_registras/', include('detaliu_registras.urls')),
+
+    path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
+
+    # visi app'o maršrutai po /detaliu_registras/
+    path(
+        "detaliu_registras/",
+        include(("detaliu_registras.urls", "detaliu_registras"), namespace="detaliu_registras"),
+    ),
+
+    # šaknį / nukreipiam į app'o index
+    path("", RedirectView.as_view(pattern_name="detaliu_registras:index", permanent=False)),
 ]
-
-# Aptarnauti static failus per development serverį (tik kai DEBUG = True)
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
